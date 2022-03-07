79ce7fd3dfbc9859b1f271db681958da942ad8ed
NMCruiser <keramsey@users.noreply.github.com>
2022-03-07 12:08:42 -0700

    Fix permissions, remove ignore all deps, remove interaction.

--- drupal/update-composer.sh
+++ drupal/update-composer.sh
@@ -9,35 +9,37 @@ composer clearcache
 # Install drush
 composer require drush/drush:^10 --ignore-platform-req=ext-gd --no-update -q
 # Install bibcite module prerequisites
-composer require -W academicpuma/citeproc-php:~1.0 adci/full-name-parser:^0.2.4 technosophos/libris:~2.0 audiolabs/bibtexparser:^1.0 caseyamcl/php-marc21:~1.0 --ignore-platform-req=ext-gd --no-update -q
+composer require -w academicpuma/citeproc-php:~1.0 adci/full-name-parser:^0.2.4 technosophos/libris:~2.0 audiolabs/bibtexparser:^1.0 caseyamcl/php-marc21:~1.0 --ignore-platform-req=ext-gd --no-update -q
 # Install bibcite module
-composer require -W drupal/bibcite:^2.0@beta drupal/bibcite_entity:^2.0@beta drupal/bibcite_migrate:^2.0@beta --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/bibcite:^2.0@beta drupal/bibcite_entity:^2.0@beta drupal/bibcite_migrate:^2.0@beta --ignore-platform-req=ext-gd --no-update -q
 # Install bibcite_migrate module --- IF drupal/drupal:^9
 #composer require -W drupal/bibcite_migrate:^2.0@beta --ignore-platform-req=ext-gd --no-update -q
 # Install contrib modules
-composer require -W drupal/admin_toolbar:^3.1 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/ckeditor_responsive_plugin:^2.0 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/download_count:^2.0 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/admin_toolbar:^3.1 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/ckeditor_responsive_plugin:^2.0 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/download_count:^2.0 --ignore-platform-req=ext-gd --no-update -q
 #composer require -W drupal/drupal8_zymphonies_theme:^2.0 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/easy_breadcrumb:^2.0 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/editor_advanced_link:^2.0 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/entity:^1.2 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/entity_reference_revisions:^1.9 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/file_entity:^2.0@beta --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/google_analytics:^3.1 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/google_tag:^1.4 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/inline_entity_form:^1.0@RC --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/jquery_ui:^1.4 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/menu_block:^1.7 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/easy_breadcrumb:^2.0 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/editor_advanced_link:^2.0 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/entity:^1.2 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/entity_reference_revisions:^1.9 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/file_entity:^2.0@beta --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/google_analytics:^3.1 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/google_tag:^1.4 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/inline_entity_form:^1.0@RC --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/jquery_ui:^1.4 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/menu_block:^1.7 --ignore-platform-req=ext-gd --no-update -q
 #composer require -W drupal/nice_menus:^1.0@beta --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/profile:^1.3 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/state_machine:^1.6 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/superfish:^1.4 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/taxonomy_manager:^2.0 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/tb_megamenu:^1.6 --ignore-platform-req=ext-gd --no-update -q
-composer require -W drupal/token:^1.10 --ignore-platform-req=ext-gd --no-update -q
-#composer require -W drupal/webform:^6.0 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/profile:^1.3 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/state_machine:^1.6 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/superfish:^1.4 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/taxonomy_manager:^2.0 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/tb_megamenu:^1.6 --ignore-platform-req=ext-gd --no-update -q
+composer require -w drupal/token:^1.10 --ignore-platform-req=ext-gd --no-update -q
+#composer require -w drupal/webform:^6.0 --ignore-platform-req=ext-gd --no-update -q
 composer update --ignore-platform-req=ext-gd
+# Update Drupal core
+composer update drupal/core -W drupal/core-* --ignore-platform-req=ext-gd
 # Clear composer cache
 composer clearcache
 # Allow plugins to silence trust composer messages
--- drupal/update-drush.sh
+++ drupal/update-drush.sh
@@ -5,6 +5,8 @@
 #
 # Exit script if any command fails (non-zero value)
 set -e
+# Save current working directory to variable
+DIR=$(pwd)
 # Clear composer cache
 composer clearcache
 # Install drush
@@ -13,42 +15,78 @@ composer require drush/drush:^10 --ignore-platform-req=ext-gd
 composer require drupal/superfish:^1.4
 # Clear composer cache
 composer clearcache
-# Save current working directory to variable
-DIR=$(pwd)
 
+# Change to web folder
+cd /opt/drupal/web
 # Set Drupal website filesystem permissions
-#cd /opt/drupal/web
-#bash ./fix-permissions.sh
+./fix-permissions.sh
 
 # Change directory to site folder
 cd /opt/drupal/web/sites/dln.swclimatehub.info
 # Clear drush cache
 drush cc drush
+
 # Uninstall nice_menus theme (not supported in Drupal 9)
 drush -y pm:uninstall nice_menus
 # Uninstall dust_bartik subtheme (not supported in Drupal 9)
 #drush -y pm:uninstall dust_bartik
+
 # Install drupal8_zymphonies_theme theme (base theme)
 drush -y theme-enable drupal8_zymphonies_theme
 # Install claro (experimental) admin theme
 drush -y theme-enable claro
+
 # Set default admin theme to claro theme
 drush -y config:set system.theme admin claro
 # Enable contrib modules
 drush -y en serialization bibcite bibcite_entity bibcite_export bibcite_import bibcite_bibtex bibcite_endnote bibcite_marc bibcite_ris
+
+# Put website in maintenance mode
+drush state:set system.maintenance_mode 1
+
+# Export Drupal database to compressed file (--gzip appends .gz to file extension)
+echo "Backing up database..."
+drush -y sql-dump --gzip --result-file=/var/mysql/backups/prior/dlnswclimatehub.sql --extra-dump=--no-tablespaces
+echo "completed!"
+# Backup web folder
+echo "Backing up files..."
+tar -czf /var/mysql/backups/prior/dlnswclimatehub_web.tar.gz /opt/drupal/web
+echo "completed!"
+
+# Rebuild drupal cache
+drush cr
 # Update drupal database
 drush -y updb
 # Install dln_drupal8_zymphonies_theme theme (subtheme) after updating drupal database
 drush -y theme-enable dln_drupal8_zymphonies_theme
 # Set default theme to dln_drupal8_zymphonies_theme theme (subtheme)
 drush -y config-set system.theme default dln_drupal8_zymphonies_theme
+# Change to web folder
+cd /opt/drupal
+# Update Drupal core (requires COMPOSER_HOME being set without changing directories)
+composer update drupal/core "drupal/core-*" --with-all-dependencies
+# Change directory to site folder
+cd /opt/drupal/web/sites/dln.swclimatehub.info
 # Update drupal database (after setting default theme, otherwise white screen on Appearance admin page)
 drush -y updb
 # Rebuild drupal cache
 drush cr
+
+# Export configuration
+drush -y config:export
 # Export Drupal database to compressed file (--gzip appends .gz to file extension)
-# which updates database script used to populate mysql container
+echo "Backing up database..."
 drush -y sql-dump --gzip --result-file=/var/mysql/backups/latest/dlnswclimatehub.sql --extra-dump=--no-tablespaces
+echo "completed!"
+# Backup web folder
+echo "Backing up files..."
+tar -czf /var/mysql/backups/latest/dlnswclimatehub_web.tar.gz /opt/drupal/web
+echo "completed!"
+# Take website out of maintenance mode
+drush state:set system.maintenance_mode 0
+
+# Rebuild drupal cache
+drush cr
 # Clear drush cache
 drush cc drush
 # Change directory to previous state
