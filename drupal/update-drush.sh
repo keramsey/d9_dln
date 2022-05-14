4428f60aec7a36cb86f52142ed5ff837d0159ace
NMCruiser <keramsey@users.noreply.github.com>
2022-05-14 10:46:16 -0600

    Removed empty lines

--- drupal/update-drush.sh
+++ drupal/update-drush.sh
@@ -15,40 +15,30 @@ composer require drush/drush:^10 --ignore-platform-req=ext-gd
 composer require drupal/superfish:^1.4
 # Clear composer cache
 composer clearcache
-
 # Set Drupal website filesystem permissions
 bash /opt/drupal/web/fix-permissions.sh
-
 # Patch drupal8_zymphonies_theme.theme file (version 2.02)
 cd /opt/drupal/web/themes/contrib/drupal8_zymphonies_theme
 wget https://www.drupal.org/files/issues/2020-11-27/3185149-syntax-error-3.patch
 patch -p1 < 3185149-syntax-error-3.patch
-
 # Change directory to site folder
 cd /opt/drupal/web/sites/default
-
 # Clear drush cache
 drush cr
-
 # Uninstall nice_menus theme (not supported in Drupal 9)
 #drush -y pm:uninstall nice_menus
-
 # Uninstall dust_bartik subtheme (not supported in Drupal 9)
 drush -y pm:uninstall dust_bartik
-
 # Install drupal8_zymphonies_theme theme (base theme)
 drush -y theme-enable drupal8_zymphonies_theme
 # Install claro (experimental) admin theme
 drush -y theme-enable claro
-
 # Set default admin theme to claro theme
 drush -y config:set system.theme admin claro
 # Enable contrib modules
 drush -y en serialization bibcite bibcite_entity bibcite_export bibcite_import bibcite_bibtex bibcite_endnote bibcite_marc bibcite_ris
-
 # Put website in maintenance mode
 drush state:set system.maintenance_mode 1
-
 # Export Drupal database to compressed file (--gzip appends .gz to file extension)
 echo "Backing up database..."
 drush -y sql-dump --gzip --result-file=/var/mysql/backups/prior/dlnswclimatehub.sql --extra-dump=--no-tablespaces
@@ -58,7 +48,6 @@ echo "Backing up files..."
 tar -czf /var/mysql/backups/prior/dlnswclimatehub_web.tar.gz /opt/drupal/web
 tar -czf /var/mysql/backups/latest/dlnswclimatehub_private.tar.gz /opt/drupal/private
 echo "...completed!"
-
 # Rebuild drupal cache
 drush cr
 # Update drupal database
@@ -67,12 +56,10 @@ drush -y updb
 drush -y theme-enable dln_drupal8_zymphonies_theme
 # Set default theme to dln_drupal8_zymphonies_theme theme (subtheme)
 drush -y config-set system.theme default dln_drupal8_zymphonies_theme
-
 # Update drupal database (after setting default theme, otherwise white screen on Appearance admin page)
 drush -y updb
 # Rebuild drupal cache
 drush cr
-
 # Export configuration
 drush -y config:export
 # Remove duplicate config yml files, then import config to delete from database
@@ -90,7 +77,6 @@ tar -czf /var/mysql/backups/latest/dlnswclimatehub_private.tar.gz /opt/drupal/pr
 echo "...completed!"
 # Take website out of maintenance mode
 drush state:set system.maintenance_mode 0
-
 # Rebuild drupal cache
 drush cr
 # Clear drush cache
